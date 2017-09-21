5fcdab056919c1fab9092203182cc5943c80b94d
cgranados16 <cgranados16@gmail.com>
2017-09-21 12:44:13 -0600

    Welcome, Waifu McWaifu

--- myapp/app.js
+++ myapp/app.js
@@ -46,15 +46,6 @@ app.use('/users', users);
 app.use('/SignUp', signUp);
 app.use('/store', store);
 
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,17 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var fetch = require('node-fetch');
+
+function getUserDetails(id){
+  return fetch('http://localhost:51336/api/User/'+id)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        return json
+    });	
+}
 
 router.get(/^(.*)$/, function(req, res, next){
   var cookieuser = req.cookies.user || '';
   var _parameters = {user : cookieuser};
-  parameters = _.extend(res.locals, _parameters);
-  next();
+  if (req.cookies.user){
+  	getUserDetails(cookieuser).then(function(result) {
+         _parameters = {user : result};
+  		 parameters = _.extend(res.locals, _parameters);
+  		 next();
+    });
+  }else{
+  	parameters = _.extend(res.locals, _parameters);
+	next();
+  }
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	res.render('index', { title: 'Express'});
 });
 
 router.get('/signOut', function (req, res) {
